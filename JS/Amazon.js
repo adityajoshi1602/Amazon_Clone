824b56fd5708e080867a76400622de5a6918f4c4
Aditya joshi <adityajoshi1602@gmail.com>
2025-10-02 17:08:17 +0530

    Few Changes

---
+++ JS/Amazon.js
@@ -0,0 +1,30 @@
+console.log('hello')
+
+//making copy of product data to add new products
+const product =[{
+    image:'https://supersimple.dev/projects/amazon/images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating:{
+        starts:4.5,
+        count:87
+    },
+    price:10.90
+},
+{
+    image:'https://supersimple.dev/projects/amazon/images/products/intermediate-composite-basketball.jpg',
+    name:'Intermediate Size Basketball',
+    rating:{
+        starts:4.5,
+        count:95
+    },
+    price:20.95
+},
+{
+    image:'https://supersimple.dev/projects/amazon/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        starts:4.5,
+        count:56
+    },
+    price:7.99
+}]
