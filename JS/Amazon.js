ed5624863ea1b255331353109c5cc17275974d26
Aditya joshi <adityajoshi1602@gmail.com>
2025-10-03 11:23:49 +0530

    Added Cart.js

--- JS/Amazon.js
+++ JS/Amazon.js
@@ -1,12 +1,12 @@
 console.log('hello')
 
 //making copy of product data to add new products
-let products_html='';
+let products_html = '';
 products.forEach((products) => {
     products_html += `            <div class="product1">
                 <div class="img">
                     <img src="${products.image}"
-                        alt="sock" style="height: 166.15px; width: 140px;">
+                        alt="sock" style="height: 170px; width: 150px;">
                 </div>
                 <div class="name">
                     <span>${products.name}</span>
@@ -15,7 +15,7 @@ products.forEach((products) => {
                     <span>⭐⭐⭐⭐✨ <span class="rate">${products.rating.count}</span></span>
                 </div>
                 <div class="price_quantity" style="padding: 10px 20px;">
-                    <span>Price : $ ${products.price}</span>
+                    <span>Price : $ ${(products.price).toFixed(2)}</span>
                     <br>
                     <select style="border-radius: 5px;" name="quantity" id="quantity">
                         <option value="1">1</option>
@@ -26,9 +26,10 @@ products.forEach((products) => {
                         <option value="6">6</option>
                     </select>
                 </div>
+                <div class="added" style="color:green;font-weight:600; margin-left:20px; margin-bottom:5px; "></div>
 
-                <div class="adto_cart" style="display:flex;justify-content: center;align-items: center;">
-                    <button
+                <div class="add_to_cart" style="display:flex;justify-content: center;align-items: center;">
+                    <button class="add" data-id="${products.id}"
                         style="padding: 10px 70px; font-weight: 600; border-radius: 10px; background-color: goldenrod; cursor: pointer;">Add
                         To Cart</button>
                 </div>
@@ -36,5 +37,27 @@ products.forEach((products) => {
 
 });
 console.log(products_html);
+document.querySelector('.products').innerHTML = products_html;
 
-document.querySelector('.products').innerHTML= products_html;
+document.querySelectorAll('.add').forEach((button)=>{
+    button.addEventListener('click',()=>{
+        const product_id=button.dataset.id;
+        let match_item;
+        cart.forEach((element)=>
+        {
+            if(element.productid===product_id){
+                match_item=element;
+            }
+        });
+
+        if (match_item) {
+            match_item.quantity +=1;
+        }else{
+            cart.push({
+                productid:product_id,
+                quantity:1
+            });
+        }
+        console.log(cart);
+    });
+});
